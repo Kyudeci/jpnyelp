adaca3aa51fa62063babe813c1b167ea642d0320
Shakiem Johnson <sjohnson@nyc.yearup.org>
2019-02-07 16:58:25 -0500

    Attempted to display database objects

--- jpnyelp/views.py
+++ jpnyelp/views.py
@@ -1,6 +1,11 @@
 from __future__ import unicode_literals
 from django.shortcuts import render
 from django.views.generic import TemplateView
+from .models import Food
 # Create your views here.
+def homepage(request):
+    foods = Food.objects
+    return render(request, 'food/index.html', {'foods':foods})
+
 class HomeView(TemplateView):
     template_name = 'index.html'
